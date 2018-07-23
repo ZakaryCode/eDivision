76066159e980e017324e1cfcf495e2e03e552976
Zakary <zakarycode@gmail.com>
2018-07-23 16:58:48 +0800

    搜索功能调整

--- src/routes/Home/Content.js
+++ src/routes/Home/Content.js
@@ -6,7 +6,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {Paper, TextField, Button, withStyles} from "material-ui";
-import ReactQuill, {Quill, Mixin, Toolbar} from "react-quill";
 
 import snack from "../../store/snack";
 import InputInfo from "../../components/Input/InputInfo";
@@ -16,6 +15,7 @@ const _path_ = window.require("path");
 const ipc = window
   .require("electron")
   .ipcRenderer;
+const multiline = new RegExp("\r|\n|\r\n", 'g');
 
 class Content extends Component {
   static propTypes = {
@@ -99,38 +99,25 @@ class Content extends Component {
           ipc.send("open-error-get-file-dialog");
         } else {
           setState("fileData", data);
-          data = data.split(new RegExp("\r|\n|\r\n", 'g'));
-          data = data.map((e) => {
-            let oP = document.createElement("p"),
-              oSpan = document.createElement("span");
-            oSpan.innerText = e;
-            oP.appendChild(oSpan);
-            return oP;
-          });
-          console.log(data);
         }
       });
     }
   }
 
   handleSaveFile = () => {
-    const {
-      file,
-      book,
-      bookD,
-      fileData,
-      directory,
-      directoryInput
-    } = this.state;
+    const {file, book, bookD, directory, directoryInput} = this.state;
     if (!directory) {
       snack.setMessage("请先选择输出路径!");
       directoryInput.focus();
       return;
     } else {
-      console.log(file, directory);
+      let oFileData = document
+        .getElementById("fileData")
+        .innerText;
+      console.log(file, directory, oFileData);
 
       let bookName = (book || bookD);
-      fs.writeFile(_path_.resolve(directory, bookName + ".txt"), fileData, function (err) {
+      fs.writeFile(_path_.resolve(directory, bookName + ".txt"), oFileData, function (err) {
         if (err) {
           ipc.send("open-error-get-file-dialog");
         } else {
@@ -176,47 +163,87 @@ class Content extends Component {
   }
 
   selectText = (text, all) => {
-    let input = this.state.fileDataInput,
-      v = input.value,
-      start = v.indexOf(text),
-      end;
-    if (start === -1) 
-      return; // 查询失败
-    end = start + text.length;
+    let input = document.getElementById("fileData"),
+      value = input.innerText,
+      range,
+      selection = window.getSelection();
     input.focus();
-    let range;
-    if (document.selection) { // IE typeof input.createTextRange != "undefined"
-      range = document
-        .body
-        .createTextRange();
-      range.moveToElementText(input); // input.createTextRange();
-      // 先将光标重合
-      range.moveStart("character", 0);
-      range.moveEnd("character", 0);
-      range.collapse(true);
-      range.moveEnd("character", end);
-      range.moveStart("character", start);
-      range.select();
-    } else if (window.getSelection) { // firefox, chrome typeof input.selectionStart != "undefined"
-      range = document.createRange();
-      range.selectNode(input);
-      range.setStart(input, 0);
-      range.setEnd(input, 1);
-      window
-        .getSelection()
-        .addRange(range);
-      // console.log(input, input.selectionStart, input.selectionEnd, start, end);
-      input.selectionStart = start;
-      input.selectionEnd = end
-      console.log(range, start, end, "innerText-" + document.getElementById("fileData").innerText);
-      input.setSelectionRange(start, end);
-      console.log(window.getSelection());
+    if (window.getSelection) { // firefox, chrome typeof input.selectionStart != "undefined"
+      let start = value.indexOf(text),
+        end = start + text.length,
+        nStart = value
+          .substring(0, start)
+          .split(multiline)
+          .map((e) => {
+            return e.length
+          }),
+        nEnd = value
+          .substring(0, end)
+          .split(multiline)
+          .map((e) => {
+            return e.length
+          }),
+        oStart = input.querySelector(`p:nth-child(${nStart.length})`),
+        oEnd = input.querySelector(`p:nth-child(${nEnd.length})`);
+      console.log(nStart, start, nStart.length - 1, nStart[nStart.length - 1]);
+      console.log(nEnd, end, nEnd.length - 1, nEnd[nEnd.length - 1]);
+      if (start === -1) 
+        return; // 查询失败
+      range = this.range()(oStart, nStart[nStart.length - 1], nEnd[nEnd.length - 1], oEnd);
+      // range.startOffset=nStart[nStart.length-1];range.endOffset=nEnd[nEnd.length-1]
+      console.log(range, oStart, oEnd);
+      console.log(range, window.getSelection());
+      selection.removeAllRanges();
+      selection.addRange(range);
+    } else {
+      console.error("平台存在错误,不支持该功能!");
     }
+  }
+
+  handleReplaceRedundantLine = () => {
+    this.setState({
+      fileData: this
+        .state
+        .fileData
+        .replace(new RegExp("\r+|\n+|\r\n", 'g'), "\n")
+    });
+  }
+
+  range = () => {
+    if (document.createRange) 
+      return (node, start, end, endNode) => {
+        var r = document.createRange();
+        try {
+          r.selectNodeContents(document.getElementById("fileData"));
+          // r.setEnd(endNode || node, 1); r.setStart(node, 0);
+          r.setEnd(endNode.firstChild || node.firstChild, end);
+          r.setStart(node.firstChild, start);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          return r;
+        }
+      };
+    else 
+      return (node, start, end) => {
+        var r = document
+          .body
+          .createTextRange();
+        try {
+          r.moveToElementText(node.parentNode);
+        } catch (e) {
+          return r;
+        }
+        r.collapse(true);
+        r.moveEnd("character", end);
+        r.moveStart("character", start);
+        return r;
+      };
   }
 
   render() {
     const {classes} = this.props;
-    // let list = this.state.list;
+    // let list = this.state.list; console.log(this.state.fileData);
 
     return (
       <div className="content">
@@ -301,19 +328,35 @@ class Content extends Component {
           <Button color="primary" onClick={this.handleReplaceAll}>
             替换所有
           </Button>
-          <TextField
+          <Button color="primary" onClick={this.handleReplaceRedundantLine}>
+            冗余换行
+          </Button>
+          <div
             id="fileData"
-            theme="snow"
-            formats={["bold"]}
-            type="text"
-            rows="15"
+            contenteditable="true"
             value={this.state.fileData}
-            onChange={this.handleChange("fileData")}
             inputRef={this.handleInputRef("fileDataInput")}
-            className={classes.textField}
-            margin="normal"
-            multiline
-            fullWidth/>
+            style={{
+            position: "relative"
+          }}>
+            {this
+              .state
+              .fileData
+              .split(multiline)
+              .map((e) => {
+                // e = e.replace(/\s/g, "  ");
+                return <p
+                  style={{
+                  whiteSpace: "pre-wrap",
+                  webkitMarginBefore: 0,
+                  webkitMarginAfter: 0
+                }}>
+                  {e.toString()
+                    ? e.toString()
+                    : <br/>}
+                </p>
+              })}
+          </div>
         </Paper>
       </div>
     );
@@ -334,3 +377,20 @@ const styles = (theme) => ({
 });
 
 export default withStyles(styles)(Content);
+
+/*
+<TextField
+theme="snow"
+formats={["bold"]}
+type="text"
+rows="15"
+style={{
+position: "absolute"
+}}
+value={this.state.fileData}
+onChange={this.handleChange("fileData")}
+inputRef={this.handleInputRef("fileDataInput")}
+className={classes.textField}
+margin="normal"
+multiline
+fullWidth/> */
